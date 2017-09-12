4d53ff9336f07a3c7fa3725e5b71e43506f4e7a9
Valia Dempsey <valentinaml@gmail.com>
2017-09-11 23:11:16 -0700

    Move js links to head section of index.html, add document.ready function to scripts.js to make it work for index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
-});
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
 
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
 
-jQuery("img").click(function() {
-  alert("This is an image.");
+  jQuery("img").click(function() {
+    alert("This is an image.");
+  });
 });
